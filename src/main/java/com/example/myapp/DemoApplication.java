decd4ad3db1bcfda573c20a60bd1891dde7aa327
unknown <asdfasfa>
2019-04-28 11:26:53 +0530

    minor changes

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -11,12 +11,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 @SpringBootApplication
 public class DemoApplication {
 
-	@Bean
-	public LocaleResolver localeResolver() {
-	    SessionLocaleResolver slr = new SessionLocaleResolver();
-	    slr.setDefaultLocale(Locale.FRANCE);
-	    return slr;
-	}
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
