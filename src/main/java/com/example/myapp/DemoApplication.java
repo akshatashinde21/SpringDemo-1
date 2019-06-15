a0f3d108dc18a47647e221fca6c64b53a7e1a49f
akshatashinde <49578752+akshsh@users.noreply.github.com>
2019-06-14 23:31:15 -0700

    Update DemoApplication.java

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -15,6 +15,8 @@ public class DemoApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		System.out.println("This is MainApplication page");
 	}
 
 }
